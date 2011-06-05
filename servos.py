1006aaad1475d076e96ae0150fe22e9df8f1613f
Brad Pitcher <bradpitcher@gmail.com>
2011-06-05 12:28:25 -0700

    pythonified servos.py

--- servos.py
+++ servos.py
@@ -1,108 +1,66 @@
-/**
- * Servo outline library
- *
- * Authors:
- *   - Eero 'rambo' af Heurlin 2010-
- *
- * License: LGPL 2.1
- */
+"""
+ Servo outline library
 
-use <triangles.scad>
+ Authors:
+   - Eero 'rambo' af Heurlin 2010-
 
-/**
- * Align DS420 digital servo
- *
- * @param vector position The position vector
- * @param vector rotation The rotation vector
- * @param boolean screws If defined then "screws" will be added and when the module is differenced() from something if will have holes for the screws
- * @param number axle_lenght If defined this will draw "backgound" indicator for the main axle
- */
-module alignds420(position, rotation, screws = 0, axle_lenght = 0)
-{
-	translate(position)
-	{
-		rotate(rotation)
-	    {
-			union()
-			{
-				// Main axle
-				translate([0,0,17])
-				{
-					cylinder(r=6, h=8, $fn=30);
-					cylinder(r=2.5, h=10.5, $fn=20);
-				}
-				// Box and ears
-				translate([-6,-6,0])
-				{
-					cube([12, 22.8,19.5], false);
-					translate([0,-5, 17])
-					{
-						cube([12, 7, 2.5]);
-					}
-					translate([0, 20.8, 17])
-					{
-						cube([12, 7, 2.5]);
-					}
-				}
-				if (screws > 0)
-				{
-					translate([0,(-10.2 + 1.8),11.5])
-					{
-						# cylinder(r=1.8/2, h=6, $fn=6);
-					}
-					translate([0,(21.0 - 1.8),11.5])
-					{
-						# cylinder(r=1.8/2, h=6, $fn=6);
-					}
+ License: LGPL 2.1
+"""
 
-				}
-				// The large slope
-				translate([-6,0,19])
-				{
-					rotate([90,0,90])
-					{
-						triangle(4, 18, 12);
-					}
-				}
+from openscad import *
+from triangles import *
 
-				/**
-				 * This seems to get too complex fast
-				// Small additional axes
-				translate([0,6,17])
-				{
-					cylinder(r=2.5, h=6, $fn=10);
-					cylinder(r=1.25, h=8, $fn=10);
-				}
-				// Small slope
-				difference()
-				{
-					translate([-6,-6,19.0])
-					{
-						cube([12,6.5,4]);
-					}
-					translate([7,-7,24.0])
-					{
-						rotate([-90,0,90])
-						{
-				            triangle(3, 8, 14);
-						}
-					}
+"""
+ Align DS420 digital servo
 
-				}
-				*/
-				// So we render a cube instead of the small slope on a cube
-				translate([-6,-6,19.0])
-				{
-					cube([12,6.5,4]);
-				}
-			}
-			if (axle_lenght > 0)
-			{
-				% cylinder(r=0.9, h=axle_lenght, center=true, $fn=8);
-			}
-		}
-	}
-}
+ @param vector position The position vector
+ @param vector rotation The rotation vector
+ @param boolean screws If defined then "screws" will be added and when the module is differenced() from something if will have holes for the screws
+ @param number axle_lenght If defined this will draw "backgound" indicator for the main axle
+"""
+def alignds420(position=[0, 0, 0], rotation=[0, 0, 0], screws = False, axle_length = 0):
+	result = []
+	# Main axle
+	mac1 = cylinder(r=6, h=8)
+	mac1.fn = 30
+	mac2 = cylinder(r=2.5, h=10.5)
+	mac2.fn = 20
+	result.append(translate([0,0,17], union([mac1, mac2])))
 
-// Tests:
-module test_alignds420(){alignds420(screws=1);}
+	# Box and ears
+	result.append(translate([-6,-6,0], union([
+		cube([12, 22.8,19.5], False),
+		translate([0,-5, 17], cube([12, 7, 2.5])),
+		translate([0, 20.8, 17], cube([12, 7, 2.5]))
+	])))
+	if screws:
+		ct = cylinder(r=1.8/2, h=6)
+		ct.fn = 6
+		ct.highlight = True
+		result += [
+			translate([0,(-10.2 + 1.8),11.5], ct),
+			translate([0,(21.0 - 1.8),11.5], ct)
+		]
+
+	# The large slope
+	result.append(
+		translate([-6,0,19],
+			rotate([90,0,90], triangle(4, 18, 12))
+		)
+	)
+
+	# Render a cube for simplicity instead of the small slope on a cube
+	result.append(translate([-6,-6,19.0], cube([12,6.5,4])))
+
+	if axle_length > 0:
+		c = cylinder(r=0.9, h=axle_length, center=True)
+		c.fn = 8
+		c.background = True
+		result.append(c)
+	return translate(position, rotate(rotation, union(result)))
+
+# Tests:
+def test_alignds420():
+	return alignds420(screws=True)
+
+#assemble(test_alignds420())
